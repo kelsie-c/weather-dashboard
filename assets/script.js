f535d0d39f7e44a5a219c5956c14bedfcca991ad
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-03-05 14:27:48 -0500

    added zip code fetch and local storage for city buttons

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,111 @@
 // use Open Weather API: One Call API & Geocoding API
+let searchValue;
+let geocodeAPIurl = "http://api.openweathermap.org/geo/1.0/";
+let oneCallAPIurl = "https://api.openweathermap.org/data/2.5/onecall?";
+let myAPIkey = "9d3d1ebc0d24d21eb01463e1e0715eb4";
+let cityButtons = document.querySelector(".savedCities");
+
+
+// use this variable to test if user input a zip code
+let zipCodeParam = /^\d{5}$/;
+
+// log user input city and send to local storage
+let searchCity = document.querySelector('.searchInput');
+let searchBtn = document.querySelector('.searchBtn');
+
+// when zip code search button is clicked, assign user input to a variable and send to api
+$('.searchbtnZip').on('click', function(event) {
+    searchValue = event.target.parentElement.parentElement.children[0].children[0].value;
+    console.log(searchValue);
+    if (zipCodeParam.test(searchValue) === true) {
+        // send zip code to geocoding api and return city, lat & lon
+        let zipSearch = geocodeAPIurl + "zip?zip=" + searchValue + ",US&appid=" + myAPIkey;
+        fetch(zipSearch)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let lat = data.lat;
+                console.log(lat);
+                let lon = data.lon;
+                console.log(lon);
+                // create an object with the lattitude and longitude
+                let obj = {
+                    lat,
+                    lon
+                }
+                console.log(obj);
+                let city = data.name;
+                console.log(city);
+                // send city name and object to local storage & create button
+                sendToLocalStorage(city, obj);
+                getWeather(lat,lon);
+            });
+    }  
+    // else return error message - please enter a valid 5-digit zip code 
+})
+
+// send string to geocoding api and return lat & lon
+
+// Set local storage and create button
+function sendToLocalStorage(city, obj) {
+    localStorage.setItem(city, JSON.stringify(obj));
+    let newCity = document.createElement("button");
+    newCity.innerText = city;
+    newCity.classList.add("button");
+    newCity.classList.add("is-block");
+    cityButtons.append(newCity);
+}
+
+function getWeather(lat,lon) {
+    console.log(lat);
+    console.log(lon);
+    let searchURL = oneCallAPIurl + "lat=" + lat + "&lon=" + lon + "&units=imperial&appid=" + myAPIkey;
+    fetch(searchURL)
+        .then(response => response.json())
+        .then(data => console.log(data));
+}
+
+
+// convert user input to lat & lon using geocoding API & send to local storage
+
+// create a button that appears beneath the search bar that calls info from local storage when clicked
+
+// api call to get the following for current day stats:
+
+    // current temperature
+
+    // current feels like temperature
+
+    // current precipitation percentage
+
+    // current weather (sunny, cloudy, rainy, etc) & assign an icon depending on response
+
+    // humidity
+
+    // wind
+
+    // pressure
+
+    // visibility
+
+    // dew point
+
+    // uv index
+
+// api call to get the following for next three hour stats:
+
+    // temperature
+
+    // weather
+
+    // humidity
+
+    // wind
+
+// api call to get the following for each of the next 5 days:
+
+    // temperature
+
+    // weather
+
+    // wind
