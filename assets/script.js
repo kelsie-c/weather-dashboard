a8a75a5b17c0300926fba4a3406b47d11c472eba
Ghost Orchid <kelsie.cummings23@gmail.com>
2021-03-05 14:59:43 -0500

    added reset for form values and formatted buttons

--- assets/script.js
+++ assets/script.js
@@ -1,5 +1,7 @@
 // use Open Weather API: One Call API & Geocoding API
 let searchValue;
+let cityValue;
+let stateValue;
 let geocodeAPIurl = "http://api.openweathermap.org/geo/1.0/";
 let oneCallAPIurl = "https://api.openweathermap.org/data/2.5/onecall?";
 let myAPIkey = "9d3d1ebc0d24d21eb01463e1e0715eb4";
@@ -23,6 +25,9 @@ $('.searchbtnZip').on('click', function(event) {
         fetch(zipSearch)
             .then(response => response.json())
             .then(data => {
+                // reset form values
+                searchValue = event.target.parentElement.parentElement.children[0].children[0].value='';
+                
                 console.log(data);
                 let lat = data.lat;
                 console.log(lat);
@@ -44,7 +49,38 @@ $('.searchbtnZip').on('click', function(event) {
     // else return error message - please enter a valid 5-digit zip code 
 })
 
-// send string to geocoding api and return lat & lon
+$('.searchbtnName').on('click', function(event) {
+    cityValue = event.target.parentElement.parentElement.children[0].children[0].value;
+    stateValue = event.target.parentElement.parentElement.children[1].children[0].value;
+    console.log(cityValue);
+    console.log(stateValue);
+    let nameSearch = geocodeAPIurl + "direct?q=" + cityValue + "," + stateValue + ",US&appid=" + myAPIkey;
+    fetch(nameSearch)
+        .then(response => response.json())
+        .then(data => {
+            // reset form values
+            cityValue = event.target.parentElement.parentElement.children[0].children[0].value='';
+            stateValue = event.target.parentElement.parentElement.children[1].children[0].value='';
+
+            console.log(data);
+            let lat = data[0].lat;
+            console.log(lat);
+            let lon = data[0].lon;
+            console.log(lon);
+            // create an object with the lattitude and longitude
+            let obj = {
+                lat,
+                lon
+            }
+            console.log(obj);
+            let city = data[0].name;
+            console.log(city);
+            // send city name and object to local storage & create button
+            sendToLocalStorage(city, obj);
+            getWeather(lat,lon);
+        });  
+    // else return error message - please enter a valid 5-digit zip code 
+})
 
 // Set local storage and create button
 function sendToLocalStorage(city, obj) {
